df779162a2048441a8784f9a042542fc6e04532f
David Fedele <dave.fedele@gmail.com>
2014-11-17 23:24:24 -0800

    completed extend

--- src/underbar.js
+++ src/underbar.js
@@ -235,6 +235,7 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+
   };
 
 
@@ -257,6 +258,13 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    for (var i=1; i<args.length; i++){
+      for(var key in args[i]){
+        obj[key] = args[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
