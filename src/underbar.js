b53d5034832f19be7316ef068e74cfcbf0ec228d
David Fedele <dave.fedele@gmail.com>
2014-11-19 10:41:07 -0800

    some - returns false for empty list

--- src/underbar.js
+++ src/underbar.js
@@ -235,7 +235,9 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-
+    if ( collection.length === 0) {
+      return false;
+    }
   };
 
 
