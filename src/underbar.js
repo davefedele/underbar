5a1bd4e019650f9398e68713dbf52a128f3f7388
David Fedele <dave.fedele@gmail.com>
2014-11-19 11:46:59 -0800

    completed some

--- src/underbar.js
+++ src/underbar.js
@@ -235,11 +235,15 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+
     if ( collection.length === 0) {
       return false;
     }
 
-    return _.every(collection, iterator);
+    return !(_.every(collection, function(item){
+      return !iterator(item);
+    }));
   };
 
 
